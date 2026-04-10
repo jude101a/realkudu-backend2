339dd6da42669d1a69999240e74150ff98ec8874
Jude Mgbeahuruike <oortcloudltd@gmail.com>
2026-04-10 18:07:27 +0100

    added notification123

--- src/config/redis.js
+++ src/config/redis.js
@@ -3,11 +3,7 @@ import { Redis } from "ioredis";
 const redisUrl = process.env.REDIS_URL || process.env.REDIS_URI || process.env.REDIS;
 
 const redisConnection = redisUrl
-  ? { url: redisUrl }
-  : {
-      host: "127.0.0.1",
-      port: 6379,
-    };
+  
 
 export const redis = new Redis(redisConnection);
 export const redisConnectionOptions = redisConnection;
