c6e4d319adcf23986e378dee0f0d15ace210d744
Jude Mgbeahuruike <oortcloudltd@gmail.com>
2026-06-23 12:43:31 +0100

    db fix123456

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,17 @@
 import pg from "pg";
+const dbUrl =
+  process.env.DATABASE_INTERNAL_URL ||
+  process.env.DATABASE_URL;
 
+console.log("DB URL PRESENT:", !!dbUrl);
+
+if (dbUrl) {
+  const parsed = new URL(dbUrl);
+
+  console.log("DB HOST:", parsed.hostname);
+  console.log("DB DATABASE:", parsed.pathname);
+  console.log("DB USER:", parsed.username);
+}
 const { Pool } = pg;
 console.log("***** NEW DB FILE LOADED *****");
 const pool = new Pool({
